9d0c29c84bc8dcb775412daeb57ae970117d0a86
Elliot Jordan and Tiffany Nguyen <tiffany@>
2020-01-14 14:27:58 -0800

    make initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".java-click").click(function() {
+    $("#java-show").toggle();
+    $("#java-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".oper-click").click(function() {
+    $("#oper-show").toggle();
+    $("#oper-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".vari-click").click(function() {
+    $("#vari-show").toggle();
+    $("#vari-hidden").toggle();
+  });
+});
